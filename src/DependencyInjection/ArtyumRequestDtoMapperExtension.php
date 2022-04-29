8ecda7de0fa177a2faf83ede87e3aadff9168faf
Artyum Petrov <artyum@protonmail.com>
2022-04-29 21:51:27 +0400

    Updated ArtyumRequestDtoMapperExtension

--- src/DependencyInjection/ArtyumRequestDtoMapperExtension.php
+++ src/DependencyInjection/ArtyumRequestDtoMapperExtension.php
@@ -4,19 +4,17 @@ namespace Artyum\RequestDtoMapperBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 class ArtyumRequestDtoMapperExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\PhpFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader = new PhpFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
         $loader->load('services.php');
-
-        $container->setParameter('artyum_request_dto_mapper.enabled', $config['enabled']);
     }
 }
