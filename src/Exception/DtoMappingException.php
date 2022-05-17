fb9121a9dbfa5be5bf69fc880214cdca751c8948
Artyum Petrov <artyum@protonmail.com>
2022-05-17 20:43:18 +0400

    Added default message in DtoMappingException

--- src/Exception/DtoMappingException.php
+++ src/Exception/DtoMappingException.php
@@ -3,10 +3,15 @@
 namespace Artyum\RequestDtoMapperBundle\Exception;
 
 use Exception;
+use Throwable;
 
 /**
  * Thrown when the request data couldn't be mapped to the DTO.
  */
 class DtoMappingException extends Exception
 {
+    public function __construct(string $message = 'Failed to map the extracted request data to the DTO.', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
